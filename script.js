30135efd427d743c885e846f2cb6621d113f167b
itzsoccer123 <68629011+itzsoccer123@users.noreply.github.com>
2023-05-14 15:02:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -467,11 +467,13 @@ fill(255,255,255)
         fill(255,0,200)
         stroke(115, 3, 3)
         text("YOU WIN",56,29)
+	    noLoop();
     }else if (pac.lives === 0) {
         textSize(27) 
         fill(255,0,200)
         stroke(115, 3, 3)
         text("Game Over",56,29)
+	 noLoop();
     
     }else {
     	 textSize(27) 
