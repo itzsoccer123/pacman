c6375ef28f15d26bdc187abf450553d488c06cbd
itzsoccer123 <68629011+itzsoccer123@users.noreply.github.com>
2023-05-14 14:59:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -473,6 +473,12 @@ fill(255,255,255)
         stroke(115, 3, 3)
         text("Game Over",56,29)
     
+    }else {
+    	 textSize(27) 
+        fill(255,0,200)
+        stroke(115, 3, 3)
+        text("Score: "+score,56,29)
+    
     }
     pac.show()
     if (pac.x <= 55) {
